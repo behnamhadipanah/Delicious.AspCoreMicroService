ec752b07a7c0487ee673e32241fec5d84fac9038
behnamhadipanah <behnamhadipanah@gmail.com>
2023-03-26 14:23:30 +0330

    Update CatalogApi

---
+++ Services/Catalog/Catalog.Api/Controllers/CatalogController.cs
@@ -0,0 +1,85 @@
+﻿using System.Net;
+using Amazon.Runtime.Internal.Auth;
+using Catalog.Api.Entities;
+using Catalog.Api.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        #region constructor
+
+        private readonly IProductRepository _productRepository;
+        private readonly ILogger<CatalogController> _logger;
+
+        public CatalogController(IProductRepository productRepository, ILogger<CatalogController> logger)
+        {
+            _productRepository = productRepository;
+            _logger = logger;
+        }
+
+
+        #endregion
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> Getproducts()
+        {
+            var result = await _productRepository.GetProducts();
+            return Ok(result);
+        }
+
+        [HttpGet("{id:length(24)}", Name = "GetProduct")]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<IActionResult> Getproduct(string id)
+        {
+            var result = await _productRepository.GetProduct(id);
+            if (result is null)
+            {
+                _logger.LogError($"Product With Id: {id} is not found");
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+
+        [HttpGet("[action]/{category}")]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<IActionResult> GetProductByCategory(string category)
+        {
+            var result = await _productRepository.GetProductsByCategory(category);
+            if (result is null)
+            {
+                _logger.LogError($"Product With category: {category} is not found");
+                return NotFound();
+            }
+            return Ok(result);
+        }
+        [HttpPost]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> CreateProduct([FromBody] Product product)
+        {
+            await _productRepository.CreateProduct(product);
+            return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+        }
+
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> UpdateProduct([FromBody] Product product)
+        {
+            return Ok(await _productRepository.UpdateProduct(product));
+        }
+        [HttpDelete("{id:length(24)}",Name = "DeleteProduct")]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteProduct(string id)
+        {
+            return Ok(await _productRepository.DeleteProduct(id));
+        }
+    }
+}
--- Services/Catalog/Catalog.Api/Program.cs
+++ Services/Catalog/Catalog.Api/Program.cs
@@ -1,4 +1,7 @@
 
+using Catalog.Api.Data;
+using Catalog.Api.Repositories;
+
 namespace Catalog.Api
 {
     public class Program
@@ -7,13 +10,12 @@ namespace Catalog.Api
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            builder.Services.AddScoped<ICatalogContext, CatalogContext>();
+            builder.Services.AddTransient<IProductRepository, ProductRepository>();
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
