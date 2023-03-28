188a98fdeb03241600919bc26930dfffa111f01d
BehnamHadipanah <behnamhadipanah@gmail.com>
2023-03-29 01:16:02 +0430

    Add Discount.Api

---
+++ Services/Discount/Discount.Api/Repositories/DiscountRepository.cs
@@ -0,0 +1,66 @@
+﻿using Dapper;
+using Discount.Api.Entities;
+using Npgsql;
+
+namespace Discount.Api.Repositories;
+
+public class DiscountRepository : IDiscountRepository
+{
+    #region Constructor
+
+    private readonly IConfiguration _configuration;
+
+    public DiscountRepository(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    #endregion
+
+    public async Task<Coupon> GetDiscount(string productName)
+    {
+        using var connection = new NpgsqlConnection
+            (_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+        var coupon = await connection.QueryFirstOrDefaultAsync<Coupon>(
+            "SELECT * FROM Coupon WHERE ProductName=@ProductName",
+            new { ProductName = productName });
+
+        if (coupon is null)
+            return new Coupon()
+            {
+                ProductName = "No Discount",
+                Description = "No Discount Description",
+                Amount = 0
+            };
+
+        return coupon;
+
+    }
+
+
+    public async Task<bool> CreateDiscount(Coupon coupon)
+    {
+        using var connection = new NpgsqlConnection
+            (_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+       var result= await connection.ExecuteAsync(
+            "INSERT INTO Coupon (ProductName,Description,Amount) VALUES (@ProductName,@Description,@Amount)",
+            new {ProductName=coupon.ProductName,Description=coupon.Description,Amount=coupon.Amount});
+       if (result == 0) return false;
+
+       return true;
+
+    }
+
+    public Task<bool> UpdateDiscount(Coupon coupon)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteDiscount(string productName)
+    {
+        throw new NotImplementedException();
+    }
+
+    
+}
