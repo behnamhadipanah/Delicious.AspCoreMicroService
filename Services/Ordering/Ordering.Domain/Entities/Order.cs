3891839b6df43f71532cde068e53eb0c0a7bd2f5
behnamhadipanah <behnamhadipanah@gmail.com>
2023-04-08 15:37:29 +0330

    Start OrderingApi

---
+++ Services/Ordering/Ordering.Domain/Entities/Order.cs
@@ -0,0 +1,19 @@
+﻿using Ordering.Domain.Common;
+
+namespace Ordering.Domain.Entities;
+
+public class Order:EntityBase
+{
+    public string Username { get; set; }
+    public decimal TotalPrice  { get; set; }
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string EmailAddress { get; set; }
+    public string Country { get; set; }
+    public string City { get; set; }
+
+    public string BankName { get; set; }
+    public string RefCode { get; set; }
+    public int PaymentMethod { get; set; }
+}
