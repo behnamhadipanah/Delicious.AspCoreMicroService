2b67a1a07140dfbf0b4df1fc21b8708f58d7f7b8
Behnam Hadipanah <behnamhadipanah@outlook.com>
2023-06-02 13:58:20 +0330

    Shopping.Aggregator

---
+++ ApiGateways/Shopping.Aggregator/Models/ShoppingModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Shopping.Aggregator.Models;
+
+public class ShoppingModel
+{
+    public string Username { get; set; }
+    public BasketModel  BasketWithProduct { get; set; }
+    public IEnumerable<OrderResponseModel>  Orders { get; set; }
+
+}
---
+++ ApiGateways/Shopping.Aggregator/Services/Implementation/OrderService.cs
@@ -0,0 +1,20 @@
+﻿using Shopping.Aggregator.Extensions;
+using Shopping.Aggregator.Models;
+
+namespace Shopping.Aggregator.Services.Implementation;
+
+public class OrderService : IOrderService
+{
+    private readonly HttpClient _client;
+
+    public OrderService(HttpClient client)
+    {
+        _client = client;
+    }
+
+    public async Task<IEnumerable<OrderResponseModel>> GetOrderByUserName(string username)
+    {
+        var response = await _client.GetAsync($"/api/v1/Order/{username}");
+        return await response.ReadContentAs<List<OrderResponseModel>>();
+    }
+}
